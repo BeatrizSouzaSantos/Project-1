b9cb129c28cac8c23a76ee918b4a28bc0dc2174e
Beatriz Souza Santos <beatrizsouzasantos1288@gmail.com>
2021-07-19 15:00:18 -0300

    arrumando linhas da tabela

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items{
+            if item.hasPrefix("nssl"){
+            //Imagem para carregar
+                pictures.append(item)
+        }
+      }
+            print(pictures)
     }
-
+        //numero de linhas da tabela
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return pictures.count
+        }
+    
+    //numero de celulas
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
 
 }
-
